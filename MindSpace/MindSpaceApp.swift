17a2a08dd3fbccd853f6d4f8973b50d46cf900ac
yousolb <yousol@umich.edu>
2024-05-05 21:09:47 -0700

    Initial Commit

---
+++ MindSpace/MindSpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindSpaceApp.swift
+//  MindSpace
+//
+//  Created by yousol bae on 5/5/24.
+//
+
+import SwiftUI
+
+@main
+struct MindSpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
